2fbaa2925947c7a64a52953725760dff86839a4c
vwital <wital128@tut.by>
2023-12-24 23:28:30 +0300

    feat: stack

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.s = 0;
+    this.memory = [];
+  }
+  push(element) {
+    this.head = element;
+    this.memory.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.head = this.memory[1];
+    return this.memory.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head;
   }
 }
 
 module.exports = {
-  Stack
+  Stack,
 };
